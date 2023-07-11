4e0d9d5013723da68c8a2d1c8b7205b854d53f7c
Arseniy Olevskiy <77734966+arolus@users.noreply.github.com>
2023-07-11 08:52:54 +0500

    regular commit

--- plugin.php
+++ plugin.php
@@ -10,11 +10,11 @@ License: GPLv2 or later
 Text Domain: ci_numbers_to_words_converter
 */
 
-if (!function_exists('add_shortcode')) die("No direct call");
+if (!function_exists('add_shortcode')) return "No direct call for Numbers to Words Converter by Calculator.iO";
 
 function display_ci_numbers_to_words_converter(){
     $page = 'index.html';
-    return '<h2><a href="https://www.calculator.io/numbers-to-words-converter/" target="_blank"><img src="' . plugins_url('assets/images/icon-48.png', __FILE__ ) . '" width="48" height="48"></a> Numbers to Words Converter</h2><div><iframe style="background:transparent; overflow: scroll" src="' . plugins_url($page, __FILE__ ) . '" width="100%" frameBorder="0" allowtransparency="true" onload="this.style.height = this.contentWindow.document.documentElement.scrollHeight + \'px\';" id="ci_numbers_to_words_converter_iframe"></iframe></div>';
+    return '<h2><a href="https://www.calculator.io/numbers-to-words-converter/" target="_blank"><img src="' . esc_url(plugins_url('assets/images/icon-48.png', __FILE__ )) . '" width="48" height="48"></a> Numbers to Words Converter</h2><div><iframe style="background:transparent; overflow: scroll" src="' . esc_url(plugins_url($page, __FILE__ )) . '" width="100%" frameBorder="0" allowtransparency="true" onload="this.style.height = this.contentWindow.document.documentElement.scrollHeight + \'px\';" id="ci_numbers_to_words_converter_iframe"></iframe></div>';
 }
 
 add_shortcode( 'ci_numbers_to_words_converter', 'display_ci_numbers_to_words_converter' );
