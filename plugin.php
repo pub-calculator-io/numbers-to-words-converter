9d535dcd69c42cdd134b3f4f528089c97cb415ed
Arseniy Olevskiy <arsidian@gmail.com>
2023-10-24 15:53:56 +0400

    regular commit

--- plugin.php
+++ plugin.php
@@ -10,6 +10,8 @@ License: GPLv2 or later
 Text Domain: ci_numbers_to_words_converter
 */
 
+if (!defined('ABSPATH')) exit;
+
 if (!function_exists('add_shortcode')) return "No direct call for Numbers to Words Converter by Calculator.iO";
 
 function display_ci_numbers_to_words_converter(){
