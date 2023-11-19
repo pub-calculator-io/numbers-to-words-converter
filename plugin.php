e9a3c71161b606e53c5bca6571416caaea5a4137
Arseniy Olevskiy <arsidian@gmail.com>
2023-11-19 13:27:03 +0400

    calculator changes commit

--- plugin.php
+++ plugin.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: Numbers to Words Converter by Calculator.iO
+Plugin Name: Numbers to Words Converter by www.calculator.io
 Plugin URI: https://www.calculator.io/numbers-to-words-converter/
 Description: Convert numbers to words with our calculator. It accepts decimals and scientific notation and can display any U.S. dollar amount in words.
 Version: 1.0.0
@@ -16,7 +16,7 @@ if (!function_exists('add_shortcode')) return "No direct call for Numbers to Wor
 
 function display_ci_numbers_to_words_converter(){
     $page = 'index.html';
-    return '<h2><a href="https://www.calculator.io/numbers-to-words-converter/" target="_blank"><img src="' . esc_url(plugins_url('assets/images/icon-48.png', __FILE__ )) . '" width="48" height="48"></a> Numbers to Words Converter</h2><div><iframe style="background:transparent; overflow: scroll" src="' . esc_url(plugins_url($page, __FILE__ )) . '" width="100%" frameBorder="0" allowtransparency="true" onload="this.style.height = this.contentWindow.document.documentElement.scrollHeight + \'px\';" id="ci_numbers_to_words_converter_iframe"></iframe></div>';
+    return '<h2><img src="' . esc_url(plugins_url('assets/images/icon-48.png', __FILE__ )) . '" width="48" height="48">Numbers to Words Converter</h2><div><iframe style="background:transparent; overflow: scroll" src="' . esc_url(plugins_url($page, __FILE__ )) . '" width="100%" frameBorder="0" allowtransparency="true" onload="this.style.height = this.contentWindow.document.documentElement.scrollHeight + \'px\';" id="ci_numbers_to_words_converter_iframe"></iframe></div>';
 }
 
 add_shortcode( 'ci_numbers_to_words_converter', 'display_ci_numbers_to_words_converter' );
